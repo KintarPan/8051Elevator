33dc7839ffcee6554b747f8fd3d442fdc53f5bd4
初時雨🍥 <Hatsushigure.c@gmail.com>
2025-12-09 02:10:57 +0800

    🦄 refactor(main): Move lists into code seg
    Program size to 1570 (And data size to 65)

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ enum WorkState
 void getContent(
     uint8_t contentLength,
     uint8_t promptLength,
-    uint8_t* nextPrompt,
+    const uint8_t code* nextPrompt,
     uint8_t nextPromptSize,
     uint8_t nextState,
     bit isPassword
@@ -46,19 +46,25 @@ void varifyPassword();
 void wrongPasswordDelay();
 void finishDelay();
 
-const uint8_t castTable[] = {
+const uint8_t code castTable[] = {
     DC_0, DC_1, DC_2, DC_3, DC_4, DC_5, DC_6, DC_7, DC_8, DC_9, DC_A, DC_B, DC_C, DC_D, DC_E, DC_F
 };
-const uint8_t truePassword[] = {1, 1, 4, 5, 1, 4};
-const uint8_t personPrompt[] = {DC_N, DC_O& DS_Dot};
-const uint8_t weightPrompt[] = {DC_R, DC_L& DS_Dot};
-const uint8_t finishPrompt[] = {DC_F, DC_1, DC_N, DC_1, DC_5, DC_H};
-uint8_t errorPrompt[] = {DC_E, DC_R, DC_R, DC_O, DC_R, DS_Disabled, DS_Disabled, DS_Disabled};
+
+// Prompts and Password
+const uint8_t code truePassword[] = {1, 1, 4, 5, 1, 4};
+const uint8_t code personPrompt[] = {DC_N, DC_O& DS_Dot};
+const uint8_t code weightPrompt[] = {DC_R, DC_L& DS_Dot};
+const uint8_t code finishPrompt[] = {DC_F, DC_1, DC_N, DC_1, DC_5, DC_H};
+const uint8_t code lockedPrompt[] = {DC_L, DC_O, DC_C, DC_A, DC_E, DC_D, DS_Disabled, DS_Disabled};
+const uint8_t code errorPrompt[] = {DC_E, DC_R, DC_R, DC_O, DC_R};
+
+// State machine variables
+uint8_t workState;
+uint8_t passwordTrialCount = 5;
 uint8_t wrongPasswordDelayTime;
 uint8_t finishDelayTime = 80;
 uint8_t maxPerson;
 uint8_t maxWeight;
-uint8_t passwordTrialCount = 5;
 uint8_t workState;
 
 int main()
@@ -106,7 +112,6 @@ void varifyPassword()
     uint8_t i = 0;
     bit passwordIsCorrect = 1;
     passwordTrialCount--;
-    errorPrompt[7] = castTable[passwordTrialCount];
     if (numberInput.currentIndex != 6)
         passwordIsCorrect = 0;
     else
@@ -123,6 +128,7 @@ void varifyPassword()
     if (!passwordIsCorrect)
     {
         Display_setPrompt(&display, errorPrompt, 8);
+        display.displayBuffer[7] = castTable[passwordTrialCount];
         wrongPasswordDelayTime = 100;
         workState = WS_PasswordWrong;
     } else
@@ -150,11 +156,10 @@ void wrongPasswordDelay()
 
 void finishDelay()
 {
-    uint8_t i = 0;
     if (finishDelayTime == 0)
     {
-        for (; i != 8; i++)
-            display.displayBuffer[i] = DS_Disabled;
+        // Finish delay over, clear screen and enter idle state
+        Display_clear(&display);
         workState = WS_Free;
     }
     finishDelayTime--;
@@ -163,7 +168,7 @@ void finishDelay()
 void getContent(
     uint8_t contentLength,
     uint8_t promptLength,
-    uint8_t* nextPrompt,
+    const uint8_t code* nextPrompt,
     uint8_t nextPromptSize,
     uint8_t nextState,
     bit isPassword
